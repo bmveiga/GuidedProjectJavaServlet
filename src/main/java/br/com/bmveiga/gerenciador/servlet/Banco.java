890f9f88a72d02896fdb712b9c2e4780a7bbb92a
unknown <barbaramoreira011@gmail.com>
2022-05-24 08:50:15 -0300

    definido modelo

---
+++ src/main/java/br/com/bmveiga/gerenciador/servlet/Banco.java
@@ -0,0 +1,17 @@
+package br.com.bmveiga.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+
+	private static List<Empresa> empresas = new ArrayList<Empresa>();
+	
+	public void adiciona(Empresa empresa) {
+		Banco.empresas.add(empresa);
+	}
+
+	public List<Empresa> getEmpresas(){
+		return Banco.empresas;
+	}
+}
